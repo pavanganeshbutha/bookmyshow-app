68412f62acc015f6933c009c55ab0fe405feda2d
pavanganeshbutha <pavanganeshbutha@gmail.com>
2026-03-08 21:04:22 +0530

    Fix DB connection log statement

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,13 @@
 const app = require("./app");
+const AppDataSource = require("./data-source");
 
 // Define the port number for the server
 const PORT = 3000;
 
-(() => {
+(async () => {
   try {
+    await AppDataSource.connect();
+    console.log("DB connection open");
     // Start the server and listen on the specified port
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
