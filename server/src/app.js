5a1433457d5193aef05f0b8b0bb5512f8f48305f
pavanganeshbutha <pavanganeshbutha@gmail.com>
2026-03-09 12:20:33 +0530

    Add cors package

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,21 @@
 // Import Express.js framework
 const express = require("express");
 const healthCheckRoutes = require("./routes/health");
+const movieRoutes = require("./routes/movie");
+const cors = require("cors");
 
 // Create Express application instance
 const app = express();
 
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    methods: ["GET", "POST", "PATCH", "DELETE"],
+  }),
+);
+
 app.use(healthCheckRoutes);
+app.use("/api/v1/movies", movieRoutes);
 
 // Global error handling middleware
 app.use((err, req, res, next) => {
